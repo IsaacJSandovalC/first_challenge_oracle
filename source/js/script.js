f21d31c08669606bd1e412694a324e31187b56d9
Isaac Sandoval <isaac@MacBook-Pro-de-Isaac.local>
2022-12-20 04:07:52 -0500

    production

--- source/js/script.js
+++ source/js/script.js
@@ -16,19 +16,26 @@ function mostrarDiv() {
   var resultado = document.getElementById("resultado");
 
   if (resultado.value == "") {
-    seccion_respuesta.style.display = "none";
+    seccion_respuesta.classList.remove('visible');
+
   } else {
-    seccion_respuesta.style.display = "flex";
+    seccion_respuesta.classList.add('visible');
   }
 }
 
 document.getElementById("copiar").addEventListener("click", function () {
   var seccion_respuesta = document.getElementById("seccion-respuesta");
   var textarea = document.getElementById("resultado");
+  var textarea_input = document.getElementById("input");
   textarea.select();
 
   document.execCommand("copy");
-
-  seccion_respuesta.style.display = 'none'; // Oculta el div
-
+  textarea_input.value = ""
+  
+  seccion_respuesta.classList.remove('visible'); // Oculta el div
 });
+
+const body = document.querySelector("body,html");
+setTimeout(function () {
+  body.classList.add("visible");
+}, 500);
