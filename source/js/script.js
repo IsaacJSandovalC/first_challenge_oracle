575b69b2e6263148b4e1679945e2b39d09c85860
Isaac Sandoval <108201006+IsaacJSandovalC@users.noreply.github.com>
2022-12-20 06:21:03 -0500

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -13,18 +13,22 @@ function decodificar() {
 
 function mostrarDiv() {
   var seccion_respuesta = document.getElementById("seccion-respuesta");
+  var serccion_texto = document.getElementById("seccion-mensaje")
   var resultado = document.getElementById("resultado");
 
   if (resultado.value == "") {
     seccion_respuesta.classList.remove('visible');
 
   } else {
+    serccion_texto.classList.add('visible')
     seccion_respuesta.classList.add('visible');
   }
 }
 
 document.getElementById("copiar").addEventListener("click", function () {
   var seccion_respuesta = document.getElementById("seccion-respuesta");
+  var serccion_texto = document.getElementById("seccion-mensaje")
+
   var textarea = document.getElementById("resultado");
   var textarea_input = document.getElementById("input");
   textarea.select();
@@ -33,9 +37,17 @@ document.getElementById("copiar").addEventListener("click", function () {
   textarea_input.value = ""
   
   seccion_respuesta.classList.remove('visible'); // Oculta el div
+  serccion_texto.classList.remove('visible')
 });
 
 const body = document.querySelector("body,html");
 setTimeout(function () {
   body.classList.add("visible");
 }, 500);
+
+
+const fechaElement = document.getElementById('fecha');
+const hoy = new Date();
+const fechaISO = hoy.toISOString();
+const fecha = fechaISO.split('T')[0];
+fechaElement.textContent = fecha;
